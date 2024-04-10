d0b85c3e1074ff7002abb2e22609dbb390dec490
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-10 09:37:10 -0300

    init: initiate a spring-boot project with jpa, starter-web, devtools, lombok and flyway dependencies

---
+++ src/main/java/com/marcosparreiras/nlw_15_java/Nlw15JavaApplication.java
@@ -0,0 +1,13 @@
+package com.marcosparreiras.nlw_15_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nlw15JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nlw15JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosparreiras/nlw_15_java/Nlw15JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosparreiras.nlw_15_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nlw15JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
