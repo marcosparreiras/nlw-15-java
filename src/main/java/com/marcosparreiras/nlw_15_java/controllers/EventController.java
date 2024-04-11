d30e639f12845a4cbae56bb5f7a2672d44e6eefa
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-11 11:38:35 -0300

    refactor: add exception-entity-handler and remove exception handle responsability from controllers

--- src/main/java/com/marcosparreiras/nlw_15_java/controllers/EventController.java
+++ src/main/java/com/marcosparreiras/nlw_15_java/controllers/EventController.java
@@ -1,12 +1,13 @@
 package com.marcosparreiras.nlw_15_java.controllers;
 
-import com.marcosparreiras.nlw_15_java.controllers.dtos.ResponseErrorDTO;
-import com.marcosparreiras.nlw_15_java.exceptions.DomainException;
 import com.marcosparreiras.nlw_15_java.useCases.CreateEvent.CreateEventRequestDTO;
+import com.marcosparreiras.nlw_15_java.useCases.CreateEvent.CreateEventResponseDTO;
 import com.marcosparreiras.nlw_15_java.useCases.CreateEvent.CreateEventUseCase;
 import com.marcosparreiras.nlw_15_java.useCases.FetchEventAttendees.FetchEventAttendeesRequestDTO;
+import com.marcosparreiras.nlw_15_java.useCases.FetchEventAttendees.FetchEventAttendeesResponseDTO;
 import com.marcosparreiras.nlw_15_java.useCases.FetchEventAttendees.FetchEventAttendeesUseCase;
 import com.marcosparreiras.nlw_15_java.useCases.GetEvent.GetEventRequestDTO;
+import com.marcosparreiras.nlw_15_java.useCases.GetEvent.GetEventResponseDTO;
 import com.marcosparreiras.nlw_15_java.useCases.GetEvent.GetEventUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -32,63 +33,29 @@ public class EventController {
   private FetchEventAttendeesUseCase fetchEventAttendeesUseCase;
 
   @PostMapping("")
-  public ResponseEntity<Object> createEvent(
+  public ResponseEntity<CreateEventResponseDTO> createEvent(
     @RequestBody CreateEventRequestDTO requestDTO
-  ) {
-    try {
-      var response = this.createEventUseCase.execute(requestDTO);
-      return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    } catch (Exception e) {
-      if (e instanceof DomainException) {
-        return ResponseEntity
-          .badRequest()
-          .body(new ResponseErrorDTO(e.getMessage()));
-      }
-      e.printStackTrace();
-      return ResponseEntity
-        .badRequest()
-        .body(new ResponseErrorDTO("Internal server error"));
-    }
+  ) throws Exception {
+    var response = this.createEventUseCase.execute(requestDTO);
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 
   @GetMapping("/{eventId}")
-  public ResponseEntity<Object> getEvent(@PathVariable String eventId) {
-    try {
-      var request = new GetEventRequestDTO(eventId);
-      var response = this.getEventUseCase.execute(request);
+  public ResponseEntity<GetEventResponseDTO> getEvent(
+    @PathVariable String eventId
+  ) throws Exception {
+    var request = new GetEventRequestDTO(eventId);
+    var response = this.getEventUseCase.execute(request);
 
-      return ResponseEntity.ok().body(response);
-    } catch (Exception e) {
-      if (e instanceof DomainException) {
-        return ResponseEntity
-          .badRequest()
-          .body(new ResponseErrorDTO(e.getMessage()));
-      }
-      e.printStackTrace();
-      return ResponseEntity
-        .badRequest()
-        .body(new ResponseErrorDTO("Internal server error"));
-    }
+    return ResponseEntity.ok().body(response);
   }
 
   @GetMapping("/{eventId}/attendees")
-  public ResponseEntity<Object> fetchEventAttendees(
+  public ResponseEntity<FetchEventAttendeesResponseDTO> fetchEventAttendees(
     @PathVariable String eventId
-  ) {
-    try {
-      var request = new FetchEventAttendeesRequestDTO(eventId);
-      var response = this.fetchEventAttendeesUseCase.execute(request);
-      return ResponseEntity.ok().body(response);
-    } catch (Exception e) {
-      if (e instanceof DomainException) {
-        return ResponseEntity
-          .badRequest()
-          .body(new ResponseErrorDTO(e.getMessage()));
-      }
-      e.printStackTrace();
-      return ResponseEntity
-        .badRequest()
-        .body(new ResponseErrorDTO("Internal server error"));
-    }
+  ) throws Exception {
+    var request = new FetchEventAttendeesRequestDTO(eventId);
+    var response = this.fetchEventAttendeesUseCase.execute(request);
+    return ResponseEntity.ok().body(response);
   }
 }
---
+++ src/main/java/com/marcosparreiras/nlw_15_java/controllers/advices/ExceptionEntityHandler.java
@@ -0,0 +1,37 @@
+package com.marcosparreiras.nlw_15_java.controllers.advices;
+
+import com.marcosparreiras.nlw_15_java.controllers.dtos.ResponseErrorDTO;
+import com.marcosparreiras.nlw_15_java.exceptions.DomainException;
+import com.marcosparreiras.nlw_15_java.exceptions.EventNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionEntityHandler {
+
+  @ExceptionHandler(DomainException.class)
+  public ResponseEntity<ResponseErrorDTO> handleDomainExceptions(
+    DomainException e
+  ) {
+    if (e.getClass().equals(EventNotFoundException.class)) {
+      return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(new ResponseErrorDTO(e.getMessage()));
+    }
+
+    return ResponseEntity
+      .badRequest()
+      .body(new ResponseErrorDTO(e.getMessage()));
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ResponseErrorDTO> handleServerExceptions(
+    DomainException e
+  ) {
+    return ResponseEntity
+      .internalServerError()
+      .body(new ResponseErrorDTO("Internal server error"));
+  }
+}
